1fafcc24a406f1bfc71b05e8b9aeaca4a0bf0a40
lucaszstrr <lucaaszstr@gmail.com>
2025-02-07 11:39:06 -0300

    Readlines corretas

---
+++ exercicio_8/index.php
@@ -0,0 +1,28 @@
+<?php
+
+    $valorProduto = readline("Digite o valor do produto: R$");
+    $formaPagamento = readline("Digite a forma de pagamento: [1]Dinheiro - [2]Cartão - [3]Parcelado 2x - [4]Parcelado 3x:  ");
+
+    if($formaPagamento === '1'){
+        
+        $desconto = $valorProduto * 0.1;
+        $valorPago = $valorProduto - $desconto;
+        echo "O valor a ser pago é R$$valorPago";
+
+    }elseif($formaPagamento === '2'){
+
+        $desconto = $valorProduto * 0.05;
+        $valorPago = $valorProduto - $desconto;
+        echo "O valor a ser pago é R$$valorPago";
+
+    }elseif($formaPagamento === '3'){
+
+        echo "O valor a ser pago é R$$valorProduto";
+
+    }elseif($formaPagamento === '4'){
+
+        $juros = $valorProduto * 0.1;
+        $valorPago = $valorProduto + $juros;
+        echo "O valor a ser pago é R$$valorPago";
+
+    }
