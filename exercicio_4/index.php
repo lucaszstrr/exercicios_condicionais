7e0c25b9042d1fb3c81bdda324a33d79ae8a0c3f
lucaszstrr <lucaaszstr@gmail.com>
2025-02-07 14:38:02 -0300

    Exercício médias

--- exercicio_4/index.php
+++ exercicio_4/index.php
@@ -1,12 +1,12 @@
 <?php
 
-    $nota1 = readline();
-    $nota2 = readline();
+    $nota1 = readline("Digite a nota 1 do aluno: ");
+    $nota2 = readline("Digite a nota 2 do aluno: ");
 
     $media = ($nota1 + $nota2)/2;
 
     if($media >= 60){
-        echo "O aluno foi aprovado com média $media" .PHP_EOL;
+        echo "O aluno foi aprovado com média $media!" .PHP_EOL;
     }else{
-        echo "O aluno não foi aprovado. A média foi de $media" .PHP_EOL;
+        echo "O aluno não foi aprovado. A média foi de $media." .PHP_EOL;
     }
